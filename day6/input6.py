e3125aa7c80e1fc94303339b8b2ecbdc3fca8a86
David Nelson <davidnelson99@outlook.com>
2023-06-14 18:48:44 +0100

    start day 6

---
+++ day6/input6.py
@@ -0,0 +1,30 @@
+# %%
+def read_input(file_name):
+    with open(file_name) as f:
+        lines = [line for line in f.readlines()]
+    return lines
+
+# %%
+
+
+
+
+# %%
+
+
+
+
+# %%
+
+
+
+
+# %%
+
+
+
+
+# %%
+
+
+
